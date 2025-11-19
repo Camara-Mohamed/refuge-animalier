803628f454bd6278cb3f6203f8cfd19de3cf6887
Mohamed Camara <mohamed.camara1@student.hepl.be>
2025-11-19 04:37:50 +0100

    feat: add public navigation language selector components

---
+++ resources/views/components/public/navigation/language/selector.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <select name="lang" id="lang">
+        <option value="en">Français
+        </option>
+        <option value="fr">English
+        </option>
+        <option value="de">Deutsch
+        </option>
+        <option value="sp">Nederlands
+        </option>
+    </select>
+</div>

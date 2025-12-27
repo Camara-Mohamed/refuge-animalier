7c6b01961a1b522a8a1e1c455e572cd9565ca1c3
Mohamed Camara <mohamed.camara1@student.hepl.be>
2025-12-27 03:53:56 +0100

    feat: url translate for navigation

--- routes/public.php
+++ routes/public.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', 'fr');
 
 Route::prefix('{locale}')->middleware(SetLocale::class)->group(function (){
-    Route::get('/', function () {
+    Route::get('/'.__('public/routes.home'), function () {
         return view('home');
     })->name('home');
 
-    Route::get('/nos-animaux', function () {
+    Route::get('/'.__('public/routes.animals'), function () {
         return view('public.animals.index');
     })->name('public.animals.index');
 
-    Route::get('/a-propos', function () {
+    Route::get('/'.__('public/routes.about'), function () {
         return view('public.about');
     })->name('public.about');
 
-    Route::get('/nous-contacter', function () {
+    Route::get('/'.__('public/routes.contact'), function () {
         return view('public.contact');
     })->name('public.contact');
 });

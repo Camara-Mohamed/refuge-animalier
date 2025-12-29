9d59b7656d8ba5f6e96a7b9dfa576f855f2e46d5
Mohamed Camara <mohamed.camara1@student.hepl.be>
2025-12-29 16:23:48 +0100

    feat: section stats, card and add icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                 <x-public.navigation.link
                     href="{{ __('') }}"
                     title="{{ __('public/home.story_link_title') }}"
-                    class="text-red-strong font-bold after:content-['→'] after:pl-1 hover:underline">
+                    class="text-red-strong font-bold after:content-['→'] hover:underline">
                     {{ __('public/home.story_link_content') }}
                 </x-public.navigation.link>
             </div>
@@ -24,6 +24,22 @@
             </figure>
         </div>
     </x-public.sections.section>
+<x-public.sections.section title="{{ __('public/home.stats_title') }}">
+        <div class="grid grid-cols-4 items-center gap-6 text-center font-serif">
+            <div class="w-64 bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center
+            gap-2 text-center">
+                <div class="col-span-3 p-4 bg-blue-strong rounded-2xl flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256
+                    256" class="fill-white">
+                        <path
+                            d="M117.25,157.92a60,60,0,1,0-66.5,0A95.83,95.83,0,0,0,3.53,195.63a8,8,0,1,0,13.4,8.74,80,80,0,0,1,134.14,0,8,8,0,0,0,13.4-8.74A95.83,95.83,0,0,0,117.25,157.92ZM40,108a44,44,0,1,1,44,44A44.05,44.05,0,0,1,40,108Zm210.14,98.7a8,8,0,0,1-11.07-2.33A79.83,79.83,0,0,0,172,168a8,8,0,0,1,0-16,44,44,0,1,0-16.34-84.87,8,8,0,1,1-5.94-14.85,60,60,0,0,1,55.53,105.64,95.83,95.83,0,0,1,47.22,37.71A8,8,0,0,1,250.14,206.7Z"></path>
+                    </svg>
+                </div>
+                <h3 class="text-2xl font-bold font-serif mt-4">{{ __('public/home.stats_number_1') }}</h3>
+                <p class="font-normal">{{ __('public/home.stats_number_1_content') }}</p>
+            </div>
+        </div>
 
+</x-public.sections.section>
 
 </x-layouts.guest>

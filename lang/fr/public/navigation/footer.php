f810b60f835ccae5b84270fc005e788fec366792
Mohamed Camara <mohamed.camara1@student.hepl.be>
2025-12-29 20:10:55 +0100

    feat: footer, translate footer and add link

---
+++ lang/fr/public/navigation/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return  [
+    'footer_title' => 'Pied de page',
+    'website_name' => 'Les Pattes Heureuses',
+
+    'home' => 'Accueil',
+    'go_home' => 'Aller à la page : Accueil',
+
+    'animals' => 'Animaux',
+    'go_animals' => 'Aller à la page : Animaux',
+
+    'about' => 'À propos',
+    'go_about' => 'Aller à la page : À propos',
+
+    'contact_us' => 'Contactez-nous',
+    'go_contact_us' => 'Aller à la page : Contactez-nous',
+
+    'go_mention_legal' => 'Aller à la page mentions légales',
+    'mention_legal' => 'Mentions légales',
+
+];
---
+++ lang/nl/public/navigation/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'footer_title' => 'Voettekst',
+    'website_name' => 'Gelukkige Pootjes',
+
+    'home' => 'Home',
+    'go_home' => 'Ga naar pagina: Home',
+
+    'animals' => 'Dieren',
+    'go_animals' => 'Ga naar pagina: Dieren',
+
+    'about' => 'Over ons',
+    'go_about' => 'Ga naar pagina: Over ons',
+
+    'contact_us' => 'Contacteer ons',
+    'go_contact_us' => 'Ga naar pagina: Contacteer ons',
+
+    'go_mention_legal' => 'Ga naar pagina: Wettelijke vermeldingen',
+    'mention_legal' => 'Wettelijke vermeldingen',
+
+];

fe0ed075702eb14e58b567204914f1cef7d85c18
Mohamed Camara <mohamed.camara1@student.hepl.be>
2025-12-30 09:40:22 +0100

    feat: form composants, build animals page structure, correct footer font

---
+++ lang/fr/public/animals/animals_index.php
@@ -0,0 +1,14 @@
+<?php
+
+return  [
+    'title' => 'Nos animaux - Les Pattes Heureuses',
+
+    'section_title' => 'Nos animaux',
+    'section_subtitle' => 'Découvrez nos pensionnaires qui attendent avec impatience de trouver leur famille pour toujours.',
+
+    'animal_found' => 'animal trouvé',
+    'animals_found' => 'animals trouvés',
+    'reset' => 'Réinitialiser',
+    'not_found' => 'Aucun autre animal trouvé.',
+
+];
---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,29 @@
+@props([
+    'for' => '',
+    'id' => '',
+    'name' => '',
+    'type' => '',
+    'class_label' => '',
+    'class_input' => '',
+    'required' => 'false',
+    'placeholder' => '',
+    'class_require' => 'text-red-normal',
+    'class_error' => 'font-serif text-sm text-red-normal mt-1',
+])
+
+<div>
+    <label for="{{ $for }}" {{ $attributes->merge(['class' => $class_label]) }}>
+        {{ $label }}
+
+        @if($required)
+            <small class="{{ $class_require }}"><abbr title="{{ __('public/form.abbr_require') }}">*</abbr></small>
+        @endif
+    </label>
+
+    <input type="{{ $type }}" name="{{ $for }}" id="{{ $id }}" {{ $attributes->merge(['class' => $class_input]) }}
+    placeholder="{{ $placeholder }}">
+
+    @error($for)
+        <p class="{{ $class_error }}">{!! $message !!}</p>
+    @enderror
+</div>

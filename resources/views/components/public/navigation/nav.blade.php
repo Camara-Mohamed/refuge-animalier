384a83ff4950a46d9736cbd23dd9d1d6afa74cc1
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-02 09:45:03 +0200

    feat(i18n): translate navigation labels

--- resources/views/components/public/navigation/nav.blade.php
+++ resources/views/components/public/navigation/nav.blade.php
@@ -5,8 +5,8 @@
 <nav class="flex items-center justify-between bg-white px-20 py-4 font-sans border-b-[0.25rem] border-b-red-strong">
     <h2 class="hidden">{{ __('public/home.main_navigation') }}</h2>
 
-    <a class="text-red-strong text-2xl font-black" href="{{ route('home', app()->getLocale()) }}" title="Aller à la page
-    d'accueil">
+    <a class="text-red-strong text-2xl font-black" href="{{ route('home', app()->getLocale()) }}" title="{{__('public/navigation/header.go_home')
+    }}">
         {{ $name }}
     </a>
 

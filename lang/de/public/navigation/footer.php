be58d33565b382cfb99756b0950cda6908da5f53
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-02 09:49:32 +0200

    feat(public): add footer with translations and link

---
+++ lang/de/public/navigation/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'footer_title' => 'Fußzeile',
+    'website_name' => 'Glückliche Pfoten',
+
+    'home' => 'Startseite',
+    'go_home' => 'Zur Seite gehen: Startseite',
+
+    'animals' => 'Tiere',
+    'go_animals' => 'Zur Seite gehen: Tiere',
+
+    'about' => 'Über uns',
+    'go_about' => 'Zur Seite gehen: Über uns',
+
+    'contact_us' => 'Kontakt',
+    'go_contact_us' => 'Zur Seite gehen: Kontakt',
+
+    'go_mention_legal' => 'Zur Seite gehen: Rechtliche Hinweise',
+    'mention_legal' => 'Rechtliche Hinweise',
+
+];
---
+++ lang/en/public/navigation/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'footer_title' => 'Footer',
+    'website_name' => 'Happy Paws',
+
+    'home' => 'Home',
+    'go_home' => 'Go to page: Home',
+
+    'animals' => 'Animals',
+    'go_animals' => 'Go to page: Animals',
+
+    'about' => 'About',
+    'go_about' => 'Go to page: About',
+
+    'contact_us' => 'Contact us',
+    'go_contact_us' => 'Go to page: Contact us',
+
+    'go_mention_legal' => 'Go to legal notice page',
+    'mention_legal' => 'Legal notice',
+
+];

0604e6a4f46ae3a23735c15dac3c1a570d90b8b6
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-02 09:54:36 +0200

    feat(database): create migrations

---
+++ database/migrations/2025_12_30_132350_adoptions.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('adoptions', function (Blueprint $table) {
+            $table->id();
+            $table->date('adoption_date')->nullable();
+            $table->text('message')->nullable();
+            $table->enum('status', ['accepted', 'rejected', 'queue', 'submitted'])->default('submitted');
+            $table->foreignId('adopter_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('animal_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('adoptions');
+    }
+};

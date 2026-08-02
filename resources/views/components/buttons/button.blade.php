8a1c4f776ad421db7e2ad9e970a1faae74056e96
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-02 09:49:28 +0200

    refactor(ui): extract button into component

--- resources/views/components/buttons/button.blade.php
+++ resources/views/components/buttons/button.blade.php
@@ -1,9 +1,14 @@
 @props([
     'href' => '#',
     'title' => '',
-    'class' => '',
+    'base' => 'flex items-center justify-center px-4 py-3 rounded-lg font-bold text-base border transition-all
+    duration-200 ease-in-out',
 ])
 
-<a href="{{ $href }}" class="{{ $class }}" title="{{ $title }}">
+<a
+    href="{{ $href }}"
+    title="{{ $title }}"
+    class="{{ $base }}"
+>
     {{ $slot }}
 </a>

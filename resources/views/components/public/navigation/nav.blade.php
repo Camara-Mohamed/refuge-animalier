d288a0ba7c8908403746c72cd520bffb70dd2e1f
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-02 09:45:03 +0200

    fix(nav): add colored navigation border

--- resources/views/components/public/navigation/nav.blade.php
+++ resources/views/components/public/navigation/nav.blade.php
@@ -2,7 +2,7 @@
     'name' => __('public/home.name_website'),
 ])
 
-<nav class="flex items-center justify-between bg-white px-20 py-4 font-sans shadow">
+<nav class="flex items-center justify-between bg-white px-20 py-4 font-sans border-b-[0.25rem] border-b-red-strong">
     <h2 class="hidden">{{ __('public/home.main_navigation') }}</h2>
 
     <a class="text-red-strong text-2xl font-black" href="{{ route('home', app()->getLocale()) }}" title="Aller à la page

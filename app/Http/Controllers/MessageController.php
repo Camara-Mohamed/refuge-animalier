07eec46851e810f5548aab0ad5fe09a457d0a05a
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-18 19:35:29 +0200

    feat(mailing): update controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserRole;
 use App\Http\Requests\StoreMessageRequest;
+use App\Mail\MessageReceivedMail;
+use App\Mail\NewMessageMail;
 use App\Models\Message;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class MessageController extends Controller
 {
     public function store(StoreMessageRequest $request)
     {
-        Message::create($request->validated());
+        $message = Message::create($request->validated());
+
+        Mail::to($message->email)->send(new MessageReceivedMail($message));
+
+        $admins = User::where('role', UserRole::ADMIN)->get();
+        if ($admins->isNotEmpty()) {
+            Mail::to($admins)->send(new NewMessageMail($message));
+        }
 
         return back()->with('send', 'Votre message a été envoyé !');
     }

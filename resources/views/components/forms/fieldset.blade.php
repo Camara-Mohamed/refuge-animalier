950d7879efa864e38a0daa9467c3767a1bb6c14e
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-25 15:13:40 +0200

    fix(admin): data management page

--- resources/views/components/forms/fieldset.blade.php
+++ resources/views/components/forms/fieldset.blade.php
@@ -2,7 +2,7 @@
     'title' => '',
 ])
 
-<fieldset {{ $attributes->merge(['class' => 'flex flex-col gap-4 border border-red-strong rounded-lg px-4 pb-4']) }}>
+<fieldset {{ $attributes->merge(['class' => 'flex flex-col gap-4 border border-red-strong rounded-lg px-4 pb-4 min-w-0']) }}>
     <legend class="px-3 -ml-3 mb-4 font-serif font-bold text-red-strong">{{ $title }}</legend>
     {{ $slot }}
 </fieldset>
--- resources/views/pages/⚡data/data.blade.php
+++ resources/views/pages/⚡data/data.blade.php
@@ -6,7 +6,7 @@
 
     <h2 class="font-serif font-bold text-2xl text-blue-strong">{{ __('admin/data.index_title') }}</h2>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
         <x-forms.fieldset title="{{ __('admin/data.species_title') }}">
             <x-flash key="success_specie" />
 
